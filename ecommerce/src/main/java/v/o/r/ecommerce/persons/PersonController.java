d9ad7f1d643b304e806f7cfd598482de8ed5299e
JuanEOL <juaneteo25@gmail.com>
2024-04-30 12:30:36 -0500

    HU_list_Person/FR-OVCOM-39-JON MESSAGE:complete code,test the list person with the same name

--- ecommerce/src/main/java/v/o/r/ecommerce/persons/PersonController.java
+++ ecommerce/src/main/java/v/o/r/ecommerce/persons/PersonController.java
@@ -1,12 +1,16 @@
 package v.o.r.ecommerce.persons;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
@@ -14,6 +18,11 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import v.o.r.ecommerce.common.interfaces.persons.IPersonController;
 import v.o.r.ecommerce.common.service.BaseServiceError;
 import v.o.r.ecommerce.persons.dto.CreatePerson;
+import v.o.r.ecommerce.persons.dto.PaginationPersonDto;
+import v.o.r.ecommerce.persons.entities.PersonEntity;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -42,5 +51,23 @@ public class PersonController implements IPersonController{
         } catch (Exception e) {
             return BaseServiceError.handleException(e);
         }
+    }
+
+    @Operation(summary = "Found a product")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Found product", content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = PersonEntity.class))),
+        @ApiResponse(responseCode = "400", description = "Bad request"),
+        @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
+    @GetMapping ("/find")
+    public ResponseEntity<?> find(@ModelAttribute PaginationPersonDto paginationPersonDto){
+        try {
+            List<Map<String,Object>> foundProduct = personService.find(paginationPersonDto);
+            return ResponseEntity.status(HttpStatus.OK).body(foundProduct);
+        } catch (Exception e) {
+            return BaseServiceError.handleException(e);
+        }
+
     }
 }
--- ecommerce/src/main/java/v/o/r/ecommerce/persons/PersonService.java
+++ ecommerce/src/main/java/v/o/r/ecommerce/persons/PersonService.java
@@ -1,11 +1,21 @@
 package v.o.r.ecommerce.persons;
 
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import v.o.r.ecommerce.common.interfaces.persons.IPersonService;
 import v.o.r.ecommerce.persons.dto.CreatePerson;
+import v.o.r.ecommerce.persons.dto.PaginationPersonDto;
 import v.o.r.ecommerce.persons.entities.PersonEntity;
 import v.o.r.ecommerce.persons.repositories.PersonRepository;
 import v.o.r.ecommerce.users.UserService;
@@ -34,5 +44,80 @@ public class PersonService implements IPersonService{
         person.setUser(userService.findByIdOrFail(createPerson.user).get());
 
         return personRepository.save(person);
+    }
+
+    public List<Map<String, Object>> find(PaginationPersonDto paginationPersonDto) {
+        List<PersonEntity> listPerson = personRepository.findAll();
+
+        boolean flatten = paginationPersonDto != null && paginationPersonDto.isFlatten();
+        int limit = paginationPersonDto != null && paginationPersonDto.getLimit() != 0 ? paginationPersonDto.getLimit(): 50;
+        int offset = paginationPersonDto != null && paginationPersonDto.getOffset() != 0 ? paginationPersonDto.getLimit(): 0;
+        String sortOrder = paginationPersonDto != null ? paginationPersonDto.getSortOrder() : null;
+        String firstName = paginationPersonDto != null ? paginationPersonDto.getFirstName() : null;
+        String lastName = paginationPersonDto != null ? paginationPersonDto.getLastName(): null;
+
+        if (flatten == true) {
+            if (limit != 50 || offset != 0 || firstName != null || lastName != null) {
+                throw new IllegalArgumentException(
+                        "The PaginationUserDto object cannot have other fields besides 'flatten'.");
+            }
+
+            Map<String, Object> response = new LinkedHashMap<>();
+            response.put("context", "persons");
+            response.put("total", listPerson.size());
+            response.put("date", listPerson.stream()
+                    .map(person -> {
+                        Map<String, Object> personMap = new LinkedHashMap<>();
+                        personMap.put("id", person.getId());
+                        personMap.put("firstName", person.getFirstName());
+                        personMap.put("lastName", person.getLastName());
+                        personMap.put("Address", person.getAddress());
+                        personMap.put("city", person.getCity());
+                        personMap.put("department", person.getDepartment());
+                        personMap.put("method of pay", person.getMethodOfPay());
+                        personMap.put("phoneNumber", person.getPhoneNumber());
+                        personMap.put("user", person.getUser().getEmail());
+                        return personMap;
+                    })
+                    .collect(Collectors.toList()));
+
+            return Collections.singletonList(response);
+        }
+
+        Comparator<Map<String, Object>> sort = Comparator.comparing(m -> (String) m.get("name"));
+
+        if (sortOrder != null && sortOrder.equalsIgnoreCase("desc")) {
+            sort = sort.reversed();
+        }
+
+        Predicate<PersonEntity> firstNameFind = person -> firstName == null || person.getFirstName().toLowerCase().contains(firstName.toLowerCase());
+        Predicate<PersonEntity> lastNameFind = person -> lastName == null || person.getLastName().toLowerCase().contains(lastName.toLowerCase());
+
+        Map<String, Object> response = new LinkedHashMap<>();
+        response.put("context", "products");
+        response.put("total", listPerson.size());
+        response.put("date", listPerson.stream()
+                .filter(firstNameFind.and(lastNameFind))
+                .map(person -> {
+                    Map<String, Object> personMap = new LinkedHashMap<>();
+                    personMap.put("id", person.getId());
+                    personMap.put("firstName", person.getFirstName());
+                    personMap.put("lastName", person.getLastName());
+                    personMap.put("Address", person.getAddress());
+                    personMap.put("city", person.getCity());
+                    personMap.put("department", person.getDepartment());
+                    personMap.put("method of pay", person.getMethodOfPay());
+                    personMap.put("phoneNumber", person.getPhoneNumber());
+                    personMap.put("user", person.getUser().getEmail());
+                    return personMap;
+                })
+                .sorted(sort)
+                .skip(offset)
+                .limit(limit)
+                .collect(Collectors.toList()));
+
+        List<Map<String, Object>> resultList = new ArrayList<>();
+        resultList.add(response);
+        return resultList;
     }
 }
