db44698bbd314e65e90695e103ff94656cb7231f
VictorMMosqueraG <victormmosquerg@gmail.com>
2024-07-08 15:31:21 -0500

    create feature seed

---
+++ ecommerce/src/main/java/v/o/r/ecommerce/seed/SeedController.java
@@ -0,0 +1,24 @@
+package v.o.r.ecommerce.seed;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@RestController
+@Tag(name = "seed")
+@RequestMapping("seed")
+public class SeedController {
+    
+    @Autowired
+    private SeedService seedService;
+
+    @PostMapping("/seed")
+    public ResponseEntity<?> seed(){
+        seedService.InitializeData();
+        return ResponseEntity.ok("Data initialization successful");
+    }
+}
