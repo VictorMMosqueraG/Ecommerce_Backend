89c9e38eb15f872248fd1937a816e844c0b5056b
VictorMosquera <victormmosquerag@gmail.com>
2024-07-15 15:07:45 -0500

    feature find detail permission was created

--- ecommerce/src/main/java/v/o/r/ecommerce/common/interfaces/permission/IPermissionController.java
+++ ecommerce/src/main/java/v/o/r/ecommerce/common/interfaces/permission/IPermissionController.java
@@ -1,10 +1,12 @@
 package v.o.r.ecommerce.common.interfaces.permission;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import v.o.r.ecommerce.permission.dto.CreatePermission;
 
 public interface IPermissionController {
      public ResponseEntity<?> save(@RequestBody CreatePermission createPermission);
+     public ResponseEntity<?> findDetail(@PathVariable Long id);
 }
